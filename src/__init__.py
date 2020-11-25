dedb603efd75eca1c1d31ec7a1ff2fa72748e331
Jarrod <jmc1627@jagmail.southalabama.edu>
2020-11-25 14:21:13 -0600

    Created rough skeleton

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+'''
+    Project: CSC 426 Data Mining
+    Authors: Jarrod Carson & Trinity Stroud
+    Date created: 11/10/2020
+    Date last modified: 11/10/2020
+'''
--- src/json_parser.py
+++ src/json_parser.py
@@ -1,2 +1,6 @@
-# Authors: Jarrod Carson, Trinity Stroud
-# CSC 426 - Project
+'''
+    Project: CSC 426 Data Mining
+    Authors: Jarrod Carson & Trinity Stroud
+    Date created: 11/10/2020
+    Date last modified: 11/10/2020
+'''
